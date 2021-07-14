4e3b2c4d213adb4fe003890f147bef557b7cf71d
= <hsyoon1799@naver.com>
2021-07-14 15:56:51 +0900

    api-integrate-3

---
+++ api-integrate/src/useAsync.js
@@ -0,0 +1,59 @@
+import { useReducer, useEffect } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+function useAsync(callback, deps = []) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: false,
+  });
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const data = await callback();
+      dispatch({ type: "SUCCESS", data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log("useEffect", deps);
+    // 아래 주석을 삭제하면 경고문 뜸
+    // Users.js 에서 deps=[]으로 호출.
+    // [] => 컴포넌트가 가장 처음 렌더링 할 때만 API 를 호출하고 싶다
+
+    // eslint 설정을 다음 줄에서만 비활성화
+    // eslint-disable-next-line
+  }, deps);
+
+  return [state, fetchData];
+}
+
+export default useAsync;
