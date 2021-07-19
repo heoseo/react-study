865179eabb8b1f291de4d83227fd1197a09b3c71
= <hsyoon1799@naver.com>
2021-07-19 17:52:18 +0900

    router-tutorial-3

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Profiles from "./Profiles";
 
-function App() {
+const App = () => {
   return (
     <div>
       <ul>
@@ -14,13 +15,16 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/profiles/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
-}
+};
 
 export default App;
